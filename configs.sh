922c2e593482bb45bff3345cd969d31880f1c780
libingyao <libingyao@tju.edu.cn>
2019-06-21 13:39:11 +0800

    sphinx completed

---
+++ configs.sh
@@ -0,0 +1,9 @@
+# Set this to point to the top level of the TailBench data directory
+DATA_ROOT=/home/parallels/Documents/tailbench.inputs
+
+# Set this to point to the top level installation directory of the Java
+# Development Kit. Only needed for Specjbb
+JDK_PATH=/usr/local/opt/java/jdk1.8.0_211
+
+# This location is used by applications to store scratch data during execution.
+SCRATCH_DIR=/home/parallels/Documents/scratch
